3977ce258a7c1d78ddd08fa87904dd9e174ee33b
joker876 <42680429+joker876@users.noreply.github.com>
2022-08-18 18:43:17 +0200

    create service for workout plans

---
+++ src/app/services/plan.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+import { WorkoutPlan } from '../scripts/types';
+import { LocalStorageService } from './local-storage.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PlanService {
+  private _plans: WorkoutPlan[];
+  private _plansSubject = new Subject<WorkoutPlan[]>();
+
+  public plans = this._plansSubject.asObservable();
+
+  constructor(
+    private localStorageService: LocalStorageService
+  ) {
+    this._plans = this.localStorageService.loadFromLocalStorage('plans') as WorkoutPlan[] ?? [];
+  }
+
+  public loadPlans(): WorkoutPlan[] {
+    return [...this._plans];
+  }
+  public addPlan(newObj: WorkoutPlan): void {
+    this._plans.push(newObj);
+    this._plansSubject.next(this._plans);
+    this.localStorageService.saveToLocalStorage('plans', this._plans);
+  }
+  
+  //! static methods
+  public static reorderPlans(plans: WorkoutPlan[]): void {
+    plans.sort((a,b)=> b.last_workout.valueOf() - a.last_workout.valueOf())
+  }
+}
