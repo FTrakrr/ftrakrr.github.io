1e5772f88414f3149bf6c5b4ab6525c1188db341
joker876 <42680429+joker876@users.noreply.github.com>
2022-09-13 17:57:21 +0200

    modal now uses [modalTitle] instead of [title]
    It disaplayed a title on hover. Should name the property better next time!

--- src/app/components/modal/modal.component.ts
+++ src/app/components/modal/modal.component.ts
@@ -1,46 +1,46 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
-  selector: 'app-modal',
-  templateUrl: './modal.component.html',
-  styleUrls: ['./modal.component.scss']
+    selector: 'app-modal',
+    templateUrl: './modal.component.html',
+    styleUrls: ['./modal.component.scss']
 })
 export class ModalComponent {
-  @Input() title?: string;
+    @Input() modalTitle?: string;
 
-  @Input() isWidthMaxContent = false;
+    @Input() isWidthMaxContent = false;
 
-  @Input() cancelButtonText?: string;
-  @Input() okButtonText?: string;
-  @Input() hideButtons: boolean = false;
-
-  @Input() open?: boolean = false;
-  @Output() openChange = new EventEmitter<boolean>();
-
-  @Output() close = new EventEmitter<Event>();
-  @Output() cancel = new EventEmitter<Event>();
-  @Output() confirm = new EventEmitter<Event>();
-
-  @Input() allowBgClose = true;
-
-  constructor() { }
-
-  onClose(event: Event) {
-    this.open = false;
-    this.close.emit(event);
-    this.openChange.emit(false);
-  }
-  onCancel(event: Event) {
-    this.onClose(event);
-    this.cancel.emit(event);
-  }
-  onConfirm(event: Event) {
-    this.onClose(event);
-    this.confirm.emit(event);
-  }
-  stopBgClick(event: Event) {
-    event.stopPropagation();
-  }
+    @Input() cancelButtonText?: string;
+    @Input() okButtonText?: string;
+    @Input() hideButtons: boolean = false;
 
     @Input() okDisabled: boolean = false;
+
+    @Input() open?: boolean = false;
+    @Output() openChange = new EventEmitter<boolean>();
+
+    @Output() close = new EventEmitter<Event>();
+    @Output() cancel = new EventEmitter<Event>();
+    @Output() confirm = new EventEmitter<Event>();
+
+    @Input() allowBgClose = true;
+
+    constructor() { }
+
+    onClose(event: Event) {
+        this.open = false;
+        this.close.emit(event);
+        this.openChange.emit(false);
+    }
+    onCancel(event: Event) {
+        this.cancel.emit(event);
+        this.onClose(event);
+    }
+    onConfirm(event: Event) {
+        this.confirm.emit(event);
+        this.onClose(event);
+    }
+    stopBgClick(event: Event) {
+        event.stopPropagation();
+    }
 }
